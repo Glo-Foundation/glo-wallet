6b08895440883a2852db7f99a2344c0a5ab53409
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-05-09 11:14:50 -0400

    add cents to balance

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -8,11 +8,20 @@ type Props = {
 };
 
 export default function Balance({ glo, setGlo, yearlyYield }: Props) {
-  const formattedGlo = new Intl.NumberFormat("en-US").format(glo);
+  const formattedGlo = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(glo);
   const formattedYearlyYield = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: 0,
     maximumFractionDigits: 1,
   }).format(yearlyYield);
+
+  const splitFmtGlo = formattedGlo.split(".");
+  const fmtGloDollarPart = splitFmtGlo[0];
+  const fmtGloCentPart = splitFmtGlo[1];
 
   return (
     <div className="bg-white rounded-[20px] pt-4">
@@ -20,20 +29,22 @@ export default function Balance({ glo, setGlo, yearlyYield }: Props) {
       <div className="self-center text-[1.1rem] text-pine-700/90">Balance</div>
         <div className="flex flex-row font-semibold justify-center">
           <div className="flex flex-row text-[2.625rem] items-baseline">
-            <div>$</div>
-            <div id="gloInput" className="font-neuehaasgrotesk max-w-[226px]">
-              {formattedGlo}
+            <div className="max-w-[226px]">
+              {fmtGloDollarPart}
+            </div>
+            <div className="text-xl">
+              .{fmtGloCentPart}
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col bg-cyan-600/20 rounded-[16px] mx-1 mb-1 px-5 py-4 pt-0">
+      <div className="flex flex-col bg-cyan-600/20 rounded-[24px] mx-1 mb-1 px-5 pb-3">
         <div className="overflow-hidden">
           <div className="h-3.5 w-3.5 bg-white -rotate-45 transform origin-top-left translate-x-36"></div>
         </div>
-        <div className="flex w-full justify-between pt-2 space-y-2">
+        <div className="flex w-full justify-between space-y-2">
           <div>
-            <Image className="inline" src="/glo-logo.svg" height={28} width={28} />
+            <Image className="inline mr-2 align-botom" src="/glo-logo.svg" height={28} width={28} />
             ${formattedYearlyYield} / year
           </div>
           <EnoughToBuy yearlyYield={yearlyYield} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { getTotalYield } from "@/utils";
 
 export default function Home() {
-  const [glo, setGlo] = useState<number>(1000);
+  const [glo, setGlo] = useState<number>(1000.90);
 
   const totalDays = 365;
   const yearlyInterestRate = 0.027;
