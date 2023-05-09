9e40cecef2e020739535a459eb4ebed074b3e850
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-05-09 10:40:45 -0400

    fix formatting of embedded chickens

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -18,7 +18,7 @@ export default function Balance({ glo, setGlo, yearlyYield }: Props) {
     <div className="bg-white rounded-[20px] pt-4">
       <div className="flex flex-col space-y-2 p-4">
       <div className="self-center text-[1.1rem] text-pine-700/90">Balance</div>
-        <div className="flex flex-row font-semibold justify-between">
+        <div className="flex flex-row font-semibold justify-center">
           <div className="flex flex-row text-[2.625rem] items-baseline">
             <div>$</div>
             <div id="gloInput" className="font-neuehaasgrotesk max-w-[226px]">
@@ -28,10 +28,10 @@ export default function Balance({ glo, setGlo, yearlyYield }: Props) {
         </div>
       </div>
       <div className="flex flex-col bg-cyan-600/20 rounded-[16px] mx-1 mb-1 px-5 py-4 pt-0">
-        <div className="overflow-hidden inline-block">
+        <div className="overflow-hidden">
           <div className="h-3.5 w-3.5 bg-white -rotate-45 transform origin-top-left translate-x-36"></div>
         </div>
-        <div className="flex justify-space-between pt-2 space-y-2">
+        <div className="flex w-full justify-between pt-2 space-y-2">
           <div>
             <Image className="inline" src="/glo-logo.svg" height={28} width={28} />
             ${formattedYearlyYield} / year
