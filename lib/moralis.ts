69db1fa7d549a0aca5bbf9a0febf0c212e045116
Rad <rad@glodollar.org>
2023-05-15 15:51:28 +0200

    Moralis with transfer history api

---
+++ lib/moralis.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import {
+  EvmChain,
+  EvmErc20TransferJSON,
+  EvmErc20TransfersResponseJSON,
+} from "moralis/common-evm-utils";
+
+const instance = axios.create({
+  baseURL: "https://deep-index.moralis.io/api/v2/",
+  headers: {
+    accept: "application/json",
+    "X-API-Key": process.env.MORALIS_API_KEY,
+  },
+});
+
+// https://docs.moralis.io/web3-data-api/reference/get-erc20-transfers
+export const fetchTransactions = async (
+  address: string,
+  chain: string = EvmChain.MUMBAI.apiHex,
+  limit = 5
+) => {
+  const transfers = await instance.get<EvmErc20TransfersResponseJSON>(
+    `erc20/transfers?contract_addresses%5B0%5D=${process.env.NEXT_PUBLIC_USDGLO}&wallet_addresses%5B0%5D=${address}&chain=${chain}&limit=${limit}`
+  );
+
+  return transfers.data.result?.map((tx: EvmErc20TransferJSON) => ({
+    type: tx.from_wallet === address ? "outgoing" : "incoming",
+    ts: tx.block_timestamp,
+    value: tx.value_decimal,
+  }));
+};
