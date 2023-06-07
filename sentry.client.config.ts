aaa8224ccdaa9062ce2ee443b7c5772a08a96f3c
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-06-07 07:08:11 -0400

    set sentry envs

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,10 +2,13 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { isProd } from "@lib/utils";
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://3a0e99805b57493897f89cae698fe7e9@o4503920068263936.ingest.sentry.io/4505249228324864",
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV,
+  enabled: !isLocal(),
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
