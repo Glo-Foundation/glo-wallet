1cd826cb06a0fbb4d1ab4a83861c389a1d01d136
Rad <rad@glodollar.org>
2023-06-15 16:46:00 +0200

    Typo

--- pages/api/eas.ts
+++ pages/api/eas.ts
@@ -7,7 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.key !== process.env.WEBHOOK_KEY) {
+  if (req.query.key !== process.env.VERCEL_WEBHOOK_KEY) {
     return res.status(401).end();
   }
 
