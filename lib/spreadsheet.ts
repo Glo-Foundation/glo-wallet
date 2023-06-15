38c7796c0b83bee1b9d08054044fe470d8d3b843
Rad <rad@glodollar.org>
2023-06-15 16:56:06 +0200

    Vercel key clean up

--- lib/spreadsheet.ts
+++ lib/spreadsheet.ts
@@ -8,7 +8,7 @@ export const fetchEarlyAdoptersEmails = async () => {
 
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
-    private_key: process.env.GOOGLE_PRIVATE_KEY!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
   });
 
   await doc.loadInfo();
