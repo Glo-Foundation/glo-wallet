45ec79ba15c755679bae27f1f301b7cf80bdab6e
Garm <garm.lucassen@gmail.com>
2023-06-16 14:41:26 +0200

    think 100vh for userAuthModal dialog works slightly better?

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
   };
 
   const openUserAuthModal = () => {
-    openModal(<UserAuthModal />, "bg-transparent");
+    openModal(<UserAuthModal />, "bg-transparent max-h-[100vh]");
   };
 
   return (
