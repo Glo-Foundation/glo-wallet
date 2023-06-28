c863504b97932537df6b96f8c9ae47c469799fee
Rad <rad@glodollar.org>
2023-06-28 11:16:48 +0200

    Middleware with prohibited countries

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,39 @@ import { NextRequest } from "next/server";
 
 import { isAuthenticated } from "./lib/auth";
 
+// https://en.wikipedia.org/wiki/ISO_3166-1
+const PROHIBITED_COUNTRIES = [
+  "BY", // Belarus
+  "BA", // Bosnia And Herzegovina
+  "BI", // Burundi
+  "CF", // Central African Republic
+  "CD", // Congo, The Democratic Republic Of The
+  "CU", // Cuba
+  "IR", // Iran, Islamic Republic Of
+  "IQ", // Iraq
+  "KP", // Korea, Democratic People's Republic Of
+  "XK", // Kosovo
+  "LB", // Lebanon
+  "LY", // Libya
+  "MK", // Macedonia
+  "NI", // Nicaragua
+  "RU", // Russian Federation
+  "SO", // Somalia
+  "SS", // South Sudan
+  "SD", // Sudan
+  "SY", // Syrian Arab Republic
+  "UA", // Ukraine
+  "VE", // Venezuela
+  "YE", // Yemen
+  "ZW", // Zimbabwe
+];
+
 export async function middleware(req: NextRequest) {
+  const country = req.geo?.country || "";
+  if (PROHIBITED_COUNTRIES.includes(country)) {
+    return new Response("Blocked for legal reasons", { status: 451 });
+  }
+
   if (!["GET", "POST"].includes(req.method)) {
     return _error(401, "method not supported");
   }
