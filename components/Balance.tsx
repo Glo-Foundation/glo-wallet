9ebc1880e5ead1232f8f7884d4964993c6d6a433
Garm <garm.lucassen@gmail.com>
2023-06-30 12:31:56 +0200

    Fix margins

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -49,28 +49,29 @@ export default function Balance({
         </div>
       </div>
       {isConnected && <Actions />}
-
-      <button
-        className="flex flex-col bg-impact-bg text-impact-fg rounded-[24px] mb-1 px-5 pb-3 w-full font-normal items-baseline"
-        onClick={() => openModal(<BuyGloModal />)}
-      >
-        <div className="">
-          <div className="h-4 w-4 bg-white -rotate-45 transform origin-top-left translate-x-32"></div>
-        </div>
-        <div className="flex w-full justify-between items-center space-y-2">
-          <div className="flex items-center">
-            <Image
-              className="pb-[2px] mr-2"
-              src="/glo-logo.svg"
-              alt="glo"
-              height={28}
-              width={28}
-            />
-            {yearlyYieldFormatted} / year
+      <div className="m-1">
+        <button
+          className="flex flex-col bg-impact-bg text-impact-fg rounded-[24px] px-5 pb-3 w-full font-normal items-baseline"
+          onClick={() => openModal(<BuyGloModal />)}
+        >
+          <div className="">
+            <div className="h-4 w-4 bg-white -rotate-45 transform origin-top-left translate-x-32"></div>
           </div>
-          <EnoughToBuy yearlyYield={yearlyYield} />
-        </div>
-      </button>
+          <div className="flex w-full justify-between items-center space-y-2">
+            <div className="flex items-center">
+              <Image
+                className="pb-[2px] mr-2"
+                src="/glo-logo.svg"
+                alt="glo"
+                height={28}
+                width={28}
+              />
+              {yearlyYieldFormatted} / year
+            </div>
+            <EnoughToBuy yearlyYield={yearlyYield} />
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
