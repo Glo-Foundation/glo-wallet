7a8f163eeadf69235af5c0fa98de2367f905d5fb
Harikrishnan Shaji <hihari777@gmail.com>
2023-07-27 16:07:39 +0530

    Add more open graph tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:url" content="https://app.glodollar.org/" />
+        <meta property="og:title" content="Glo App" />
+        <meta
+          property="og:description"
+          content="Sign up to buy Glo Dollar using our app. See your transactions and the impact your Glo Dollar holdings have."
+        />
         <meta
           property="og:image"
           content="https://uploads-ssl.webflow.com/62289d6493efe7c3b765d6bd/63d146d464f48942d593bc57_Group%204%20(3).png"
         />
+        <meta property="og:site_name" content="Glo Dollar" />
       </Head>
       <body>
         <Main />
