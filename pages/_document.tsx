d7372913a2043bf8bbfe8dc9062ee4968835746a
Harikrishnan Shaji <hihari777@gmail.com>
2023-07-27 15:08:23 +0530

    Testing a simple OG tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          property="og:image"
+          content="https://uploads-ssl.webflow.com/62289d6493efe7c3b765d6bd/63d146d464f48942d593bc57_Group%204%20(3).png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
