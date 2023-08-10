abd571c9a43ecf3b46c465f112096ae52921d929
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-08-10 16:35:00 -0400

    fix up empty state test

--- tests/transactions-list/transactions-list.gui.ts
+++ tests/transactions-list/transactions-list.gui.ts
@@ -1,7 +1,7 @@
 import { test } from "@guardianui/test";
 import { expect } from "@playwright/test";
 
-import { getBaseURL } from "../utils";
+import { loginWithMetamask, getBaseURL } from "../utils";
 
 test.use({
   baseURL: getBaseURL(),
@@ -21,25 +21,7 @@ test.describe("Transactions List", () => {
 
   test.describe("empty state", () => {
     test.beforeEach(async ({ page, gui }) => {
-      const POLYGON = 137;
-      await gui.initializeChain(POLYGON, 45735806);
-      await page.goto("/");
-
-      const GLO = "0x4F604735c1cF31399C6E711D5962b2B3E0225AD3";
-      await gui.setBalance(GLO, "12340000000000000000000");
-
-      const authPopupVisible = await page.isVisible(
-        "text=Welcome to the Glo App"
-      );
-      if (!authPopupVisible) {
-        await page.getByTestId("primary-login-button").click();
-      }
-
-      const dialog = await page.waitForSelector("#modal");
-      const checkbox = await dialog.$("#tos-checkbox");
-      await checkbox.check();
-      const socialLoginButton = await dialog.$("#social-login-button");
-      await socialLoginButton.click();
+      await loginWithMetamask(page, gui);
     });
 
     test("it should render 'No transactions yet - buy some Glo?'", async ({
