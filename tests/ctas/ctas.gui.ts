bc975bde9907391f83dd4bc3a0296ae3ea726739
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-08-16 17:43:35 -0400

    add tweet test

--- tests/ctas/ctas.gui.ts
+++ tests/ctas/ctas.gui.ts
@@ -4,7 +4,7 @@ import { expect } from "@playwright/test";
 import { loginWithMetamask } from "../utils";
 
 test.describe("Help Grow CTAs", () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, gui }) => {
     await page.goto("/");
   });
 
@@ -19,6 +19,22 @@ test.describe("Help Grow CTAs", () => {
       await expect(
         page.getByRole("heading", { name: "Buy Glo Dollar Merch" })
       ).toBeVisible();
+    });
+  });
+
+  test.describe("link navs", () => {
+    test("should open tweet modal on click", async ({ page }) => {
+      const tweetCTA = await page.locator(".cta").first();
+      console.log({ tweetCTA });
+      await tweetCTA.click({ force: true });
+
+      await expect(page.getByRole("dialog")).toBeTruthy();
+      await expect(
+        page.getByRole("button", { name: "1. Tweet your impact" })
+      ).toBeTruthy();
+      await expect(
+        page.getByRole("button", { name: "2. Verify Tweet" })
+      ).toBeTruthy();
     });
   });
 });
