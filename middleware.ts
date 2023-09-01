b0eff064e51231be62dd1fe078a97111892189b0
Rad <rad@glodollar.org>
2023-09-01 15:00:56 +0200

    Rate limitting added

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,33 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { ipAddress } from "@vercel/edge";
+import { kv } from "@vercel/kv";
+import { NextResponse, NextRequest } from "next/server";
 
 import { isAuthenticated } from "./lib/auth";
 import { PROHIBITED_COUNTRIES } from "./lib/config";
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  // 5 requests from the same IP in 10 seconds
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
+});
+
+// Define which routes you want to rate limit
+export const config = {
+  matcher: "/",
+};
 
 export async function middleware(req: NextRequest) {
   const country = req.geo?.country || "";
   if (PROHIBITED_COUNTRIES.includes(country)) {
     return new Response("Blocked for legal reasons", { status: 451 });
+  }
+
+  const ip = ipAddress(req) || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) {
+    return Response.redirect(new URL("/blocked.html", req.url));
   }
 
   const path = req.nextUrl.pathname;
