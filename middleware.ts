f9d29c7ff7562684ccf9f48c1f84a97800c373fc
Rad <rad@glodollar.org>
2023-09-01 17:01:35 +0200

    Express lib leveraged

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,19 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import { ipAddress } from "@vercel/edge";
-import { kv } from "@vercel/kv";
+import { rateLimit } from "express-rate-limit";
 import { NextResponse, NextRequest } from "next/server";
 
 import { isAuthenticated } from "./lib/auth";
 import { PROHIBITED_COUNTRIES } from "./lib/config";
 
-const ratelimit = new Ratelimit({
-  redis: kv,
-  // 200 requests from the same IP in 5 seconds
-  limiter: Ratelimit.slidingWindow(200, "5 s"),
-});
+export async function middleware(req: NextRequest, res: NextResponse) {
+  try {
+    await applyMiddleware(limiter)(req, res);
+  } catch (er) {
+    return new Response("Too many requests!", { status: 429 });
+  }
 
-export async function middleware(req: NextRequest) {
   const country = req.geo?.country || "";
   if (PROHIBITED_COUNTRIES.includes(country)) {
     return new Response("Blocked for legal reasons", { status: 451 });
-  }
-
-  const ip = ipAddress(req) || "127.0.0.1";
-  const { success } = await ratelimit.limit(ip);
-
-  if (!success) {
-    return new Response("Too many requests!", { status: 429 });
   }
 
   const path = req.nextUrl.pathname;
@@ -41,3 +32,28 @@ export async function middleware(req: NextRequest) {
 
 const _error = (status: number, message: string) =>
   new NextResponse(JSON.stringify({ message }), { status });
+
+const applyMiddleware =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (middleware: any) => (request: NextRequest, response: NextResponse) =>
+    new Promise((resolve, reject) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      middleware(request, response, (result: any) =>
+        result instanceof Error ? reject(result) : resolve(result)
+      );
+    });
+
+const getIP = (request: NextRequest) =>
+  request.ip ||
+  request.headers.get("x-forwarded-for") ||
+  request.headers.get("x-real-ip")!;
+
+const limiter = rateLimit({
+  keyGenerator: getIP,
+  windowMs: 5 * 1000, // 5 seconds
+  max: 200, // Limit each IP to 200 requests per `window`
+  legacyHeaders: false,
+  handler: () => {
+    throw new Error();
+  },
+});
