26ecaf490fdb670e44a79fcaf33b55ddc955aac0
Garm <garm.lucassen@gmail.com>
2023-10-19 15:22:59 +0200

    Add a simple button to add Glo to metamask

--- components/AddToWallet.tsx
+++ components/AddToWallet.tsx
@@ -11,10 +11,11 @@ import Image from "next/image";
 import { useRef, useState } from "react";
 import { useNetwork, useSwitchNetwork } from "wagmi";
 
+import { chainConfig } from "@/lib/config";
 import { useOutsideClick } from "@/lib/hooks";
 import { getAllowedChains } from "@/lib/utils";
 
-export default function NetworkSwitcher() {
+export default function AddToWallet() {
   const { switchNetwork } = useSwitchNetwork();
   const { chain } = useNetwork();
   const [dropdownActive, setDropdownActive] = useState(false);
@@ -42,6 +43,32 @@ export default function NetworkSwitcher() {
     }
   };
 
+  const addTokenToWallet = (chain: Chain) => {
+    try {
+      window.ethereum
+        .request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20", // Initially only supports ERC20, but eventually more!
+            options: {
+              address: chainConfig[chain.id], // The address that the token is at.
+              symbol: "USDGLO", // A ticker symbol or shorthand, up to 5 chars.
+              decimals: 18, // The number of decimals in the token
+              image: "https://app.glodollar.org/glo-logo.svg", // A string url of the token logo
+            },
+          },
+        })
+        .then(() => {
+          console.log("Thanks for your interest!");
+        })
+        .catch(() => {
+          console.log("Your loss!");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getChainOptions = () => {
     const chains = getAllowedChains();
     return chains.map((chain) => (
@@ -49,6 +76,7 @@ export default function NetworkSwitcher() {
         className="hover:bg-pine-100 px-2 py-2.5 rounded-lg"
         onClick={() => {
           switchNetwork?.(chain.id);
+          addTokenToWallet(chain);
           setDropdownActive(false);
         }}
         key={chain.id}
@@ -67,7 +95,7 @@ export default function NetworkSwitcher() {
   };
 
   return (
-    <div className="relative mr-2" ref={ref}>
+    <div className="relative ml-2" ref={ref}>
       <div
         className={`rounded-lg hover:bg-white hover:drop-shadow-sm w-9 h-9 z-10 flex justify-center ${
           dropdownActive && "bg-white drop-shadow-sm"
@@ -75,7 +103,7 @@ export default function NetworkSwitcher() {
         onClick={() => setDropdownActive(!dropdownActive)}
       >
         <Image
-          src={chain ? getChainLogoPath(chain) : "/question-mark.svg"}
+          src={"/metamask.svg"}
           alt={`${chain ? chain.name : "unknown-chain"}-logo`}
           height={18}
           width={18}
