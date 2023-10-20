220df4bef5f60c9cabc630312279c68fa99fddeb
Garm <garm.lucassen@gmail.com>
2023-10-20 11:47:33 +0200

    Add to wallet for this actual project

--- components/AddToWallet.tsx
+++ components/AddToWallet.tsx
@@ -8,20 +8,19 @@ import {
   celo,
 } from "@wagmi/core/chains";
 import Image from "next/image";
-import { useRef, useState } from "react";
-import { useNetwork, useSwitchNetwork } from "wagmi";
+import { useRef, useState, useEffect } from "react";
+import { Tooltip } from "react-tooltip";
+import { useNetwork, useSwitchNetwork, useWalletClient } from "wagmi";
 
 import { chainConfig } from "@/lib/config";
 import { useOutsideClick } from "@/lib/hooks";
 import { getAllowedChains } from "@/lib/utils";
 
 export default function AddToWallet() {
-  const { switchNetwork } = useSwitchNetwork();
   const { chain } = useNetwork();
-  const [dropdownActive, setDropdownActive] = useState(false);
-
-  const ref = useRef(null);
-  useOutsideClick(ref, () => setDropdownActive(false));
+  const { data: walletClient } = useWalletClient();
+  const [isAddToWalletTooltipOpen, setIsAddToWalletTooltipOpen] =
+    useState(false);
 
   const getChainLogoPath = (chain: Chain): string => {
     switch (chain.id) {
@@ -43,19 +42,16 @@ export default function AddToWallet() {
     }
   };
 
-  const addTokenToWallet = (chain: Chain) => {
+  function addTokenToWallet(chain: Chain) {
     try {
-      window.ethereum
-        .request({
-          method: "wallet_watchAsset",
-          params: {
-            type: "ERC20", // Initially only supports ERC20, but eventually more!
-            options: {
-              address: chainConfig[chain.id], // The address that the token is at.
-              symbol: "USDGLO", // A ticker symbol or shorthand, up to 5 chars.
-              decimals: 18, // The number of decimals in the token
-              image: "https://app.glodollar.org/glo-logo.svg", // A string url of the token logo
-            },
+      walletClient
+        .watchAsset({
+          type: "ERC20", // Initially only supports ERC20, but eventually more!
+          options: {
+            address: chainConfig[chain.id], // The address that the token is at.
+            symbol: "USDGLO", // A ticker symbol or shorthand, up to 5 chars.
+            decimals: 18, // The number of decimals in the token
+            image: "https://app.glodollar.org/glo-logo.svg", // A string url of the token logo
           },
         })
         .then(() => {
@@ -67,53 +63,30 @@ export default function AddToWallet() {
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const getChainOptions = () => {
-    const chains = getAllowedChains();
-    return chains.map((chain) => (
-      <li
-        className="hover:bg-pine-100 px-2 py-2.5 rounded-lg"
-        onClick={() => {
-          switchNetwork?.(chain.id);
-          addTokenToWallet(chain);
-          setDropdownActive(false);
-        }}
-        key={chain.id}
-      >
-        <div className="flex flex-row space-x-2">
-          <Image
-            src={getChainLogoPath(chain)}
-            alt={`${chain.name}-logo`}
-            height={18}
-            width={18}
-          />
-          <div>{chain.name}</div>
-        </div>
-      </li>
-    ));
-  };
+  }
 
   return (
-    <div className="relative ml-2" ref={ref}>
+    <div className="relative ml-2">
+      <Tooltip
+        id="add-to-wallet-tooltip"
+        content="Add to wallet"
+        isOpen={isAddToWalletTooltipOpen}
+      />
       <div
-        className={`rounded-lg hover:bg-white hover:drop-shadow-sm w-9 h-9 z-10 flex justify-center ${
-          dropdownActive && "bg-white drop-shadow-sm"
-        }`}
-        onClick={() => setDropdownActive(!dropdownActive)}
+        className={`rounded-lg hover:bg-white hover:drop-shadow-sm w-9 h-9 z-10 flex justify-center`}
+        onMouseOver={() => setIsAddToWalletTooltipOpen(true)}
+        onMouseOut={() => setIsAddToWalletTooltipOpen(false)}
+        onClick={() => addTokenToWallet(chain)}
+        data-tooltip-id="add-to-wallet-tooltip"
+        data-tooltip-content="Add to wallet"
       >
         <Image
           src={"/metamask.svg"}
-          alt={`${chain ? chain.name : "unknown-chain"}-logo`}
+          alt={`metamask-logo`}
           height={18}
           width={18}
         />
       </div>
-      {dropdownActive && (
-        <div className="absolute p-2 bg-white rounded-lg w-52 drop-shadow-sm mt-1">
-          <ul>{getChainOptions()}</ul>
-        </div>
-      )}
     </div>
   );
 }
