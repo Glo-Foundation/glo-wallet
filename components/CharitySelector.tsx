70a96f3f490a0de34c2fe248fc5931597e9c0cdd
Garm <garm.lucassen@gmail.com>
2024-03-15 11:25:16 +0100

    wtf mate

--- components/CharitySelector.tsx
+++ components/CharitySelector.tsx
@@ -1,6 +1,10 @@
+import { Charity } from "@prisma/client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useAccount } from "wagmi";
 
 import CharitySelectorModal from "@/components/Modals/CharitySelectorModal";
+import { api, sliceAddress } from "@/lib/utils";
 
 type Props = {
   openModal: (content: JSX.Element) => void;
@@ -8,6 +12,31 @@ type Props = {
 };
 
 export default function CharitySelector({ openModal, yearlyYield }: Props) {
+  const { address, isConnected, connector } = useAccount();
+  const [selectedCharity, setSelectedCharity] = useState<Charity | null>(null);
+  const loggedIn = localStorage.getItem("loggedIn");
+
+  useEffect(() => {
+    console.log("loggedIn: ", loggedIn);
+    if (loggedIn && isConnected && !selectedCharity) {
+      getCurrentSelectedCharity();
+    }
+  }, [loggedIn, isConnected]);
+
+  const getCurrentSelectedCharity = async (): void => {
+    // console.log(selectedCharity);
+    // console.log("api: ", await api());
+    api()
+      .get(`/charity`)
+      .then((res) => {
+        const currentSelectedCharity = res.data[0].name as Charity;
+        setSelectedCharity(Charity[currentSelectedCharity]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div className="m-1 relative z-0 flex justify-center">
       <button
@@ -24,7 +53,7 @@ export default function CharitySelector({ openModal, yearlyYield }: Props) {
               height={16}
               alt="choose public good to fund"
             />
-            <p className="ml-2 text-sm">Charity</p>
+            <p className="ml-2 text-sm">{selectedCharity}</p>
           </div>
         </div>
       </button>
