1d883746c16f3e9d96ac1e0c3623ed0d35c2cfd8
Garm <garm.lucassen@gmail.com>
2024-06-11 12:36:24 +0200

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  webpack: (config, { isServer, buildId, dev, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        }),
+        new webpack.NormalModuleReplacementPlugin(/node:crypto/, (resource) => {
+          resource.request = resource.request.replace(/^node:/, "");
+        })
+      );
+    }
+    return config;
+  },
+
   async rewrites() {
     return [
       {
