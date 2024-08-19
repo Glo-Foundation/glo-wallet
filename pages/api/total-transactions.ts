38190cad92fa4ae8b8ab4ae39308446008ee45d7
Garm <garm.lucassen@gmail.com>
2024-08-19 10:53:33 +0200

    Oopsies

--- pages/api/total-transactions.ts
+++ pages/api/total-transactions.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as cache from "@/lib/cache";
 import { fetchTotalTransactions } from "@/lib/dune";
 
-const CACHE_KEY = "total-holders";
+const CACHE_KEY = "total-transactions";
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
