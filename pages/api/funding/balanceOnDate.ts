70e5feaa386cf92f5a4c75e3df1cd0204fb1ee16
Rad <jaaroos@gmail.com>
2024-09-06 16:00:24 +0200

    Total charity distribution

--- pages/api/funding/balanceOnDate.ts
+++ pages/api/funding/balanceOnDate.ts
@@ -1,10 +1,16 @@
 import { Charity, CharityChoice } from "@prisma/client";
 import { BigNumber } from "ethers";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Chain } from "viem";
 
 import { getBalances, getAverageBalance, getStellarTxs } from "@/lib/balance";
 import { fetchGloTransactions } from "@/lib/blockscout-explorer";
-import { getChains } from "@/lib/utils";
+import {
+  DEFAULT_CHARITY_PER_CHAIN,
+  getChains,
+  getMarketCap,
+  getStellarMarketCap,
+} from "@/lib/utils";
 import prisma from "lib/prisma";
 
 export default async function handler(
@@ -12,7 +18,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const chains = getChains();
-  const chainsObject: Record<string, any> = chains.reduce(
+  const chainsObject: Record<string, Chain> = chains.reduce(
     (a, v) => ({
       ...a,
       [["Ethereum", "Polygon"].includes(v.name)
@@ -44,10 +50,15 @@ export default async function handler(
     },
   });
 
+  const allocated: { [key: string]: BigNumber } = Object.keys(
+    chainsObject
+  ).reduce((acc, cur) => ({ ...acc, [cur]: BigNumber.from(0) }), {
+    stellar: BigNumber.from(0),
+  });
+
   const possibleFundingChoices: { [key: string]: number } = Object.keys(
     Charity
   ).reduce((acc, cur) => ({ ...acc, [cur]: 0 }), {});
-  delete possibleFundingChoices["OPEN_SOURCE"];
 
   const choicesByAddress = allFundingChoices.reduce(
     (acc, cur) => ({
@@ -138,9 +149,10 @@ export default async function handler(
           firstThisMonth,
           balancesEndOfMonth[key]
         );
-        averageTotalBalanceThisMonth = averageTotalBalanceThisMonth.add(
-          stellarBalance.mul(BigInt(10 ** 11)) // To ETH precision
-        );
+        const stellarAdjusted = stellarBalance.mul(BigInt(10 ** 11)); // To ETH precision;
+        averageTotalBalanceThisMonth =
+          averageTotalBalanceThisMonth.add(stellarAdjusted);
+        allocated["stellar"] = allocated["stellar"].add(stellarAdjusted);
       } else if (!isStellar) {
         const chainName = key.replace("Balance", "");
 
@@ -160,6 +172,8 @@ export default async function handler(
         );
         averageTotalBalanceThisMonth =
           averageTotalBalanceThisMonth.add(averageBalance);
+
+        allocated[chainName] = allocated[chainName].add(averageBalance);
       }
     }
 
@@ -174,5 +188,48 @@ export default async function handler(
     });
   }
 
-  return res.status(200).json({ possibleFundingChoices });
+  const final = await calculateRemainingPerCharity(
+    chainsObject,
+    allocated,
+    possibleFundingChoices
+  );
+
+  return res.status(200).json({ possibleFundingChoices: final });
 }
+
+const calculateRemainingPerCharity = async (
+  chains: Record<string, Chain>,
+  allocated: {
+    [key: string]: BigNumber;
+  },
+  choices: {
+    [key: string]: number;
+  }
+) => {
+  const chainObjects = Object.entries(chains).map(([key, chain]) => ({
+    id: chain.id,
+    name: key,
+  }));
+  chainObjects.push({ id: 0, name: "stellar" });
+
+  for (const chain of chainObjects) {
+    const { id, name } = chain;
+    console.log({ id, name });
+    const marketCap = await (id > 0
+      ? getMarketCap(id)
+      : getStellarMarketCap().then((x) =>
+          BigNumber.from(x).mul(BigInt(10 ** 18))
+        ));
+    const charity = DEFAULT_CHARITY_PER_CHAIN(id.toString());
+
+    console.log({
+      marketCap: marketCap.toString(),
+      allocated: allocated[name].toString(),
+    });
+    choices[charity] += marketCap
+      .sub(allocated[name])
+      .div(BigInt(10 ** 18))
+      .toNumber();
+  }
+  return choices;
+};
