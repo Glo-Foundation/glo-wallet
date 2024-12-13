f213344f32447b5955a8d31aad4a2802ca8b781c
Rad <jaaroos@gmail.com>
2024-12-13 15:18:25 +0100

    FIx redirect issue on Cb app

--- pages/purchased.tsx
+++ pages/purchased.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 // Generic callback URL to reload the balance and ctas
@@ -6,13 +7,19 @@ export default function PurchasedCallback() {
     const bc = new BroadcastChannel("glo-channel-purchased");
     bc.postMessage({ success: true });
     window.close();
+    const closed = window.closed;
+    if (!closed) {
+      push("/");
+    }
   }, []);
+
+  const { push } = useRouter();
 
   // Fallback in case window is not closed automagically
   return (
     <>
       <div>Payment completed</div>
-      <div className="cursor-pointer" onClick={() => window.close()}>
+      <div className="cursor-pointer" onClick={() => push("/")}>
         Click close to continue
       </div>
     </>
