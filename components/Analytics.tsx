a8782fb0ace7b775f9161305f097a2f3878be5e5
Rad <jaaroos@gmail.com>
2025-01-07 14:18:07 +0100

    Remove hotjar

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -9,18 +9,6 @@ export default function Analytics() {
         data-api="/pl/api/event"
         defer
       />
-      <Script id="hotjar">
-        {`
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3591238,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </Script>
     </>
   );
 }
