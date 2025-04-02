6e3c3da28018ef2e6d8f3ab042ea5544ea731d2e
Rad <jaaroos@gmail.com>
2025-04-02 20:32:37 +0200

    Show per month data for celo lp

--- pages/api/celo-lp.ts
+++ pages/api/celo-lp.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/lib/prisma";
@@ -18,6 +19,17 @@ export default async function handler(
   });
 
   const today = new Date();
+  const firstDayOfLastMonth = new Date(
+    today.getFullYear(),
+    today.getMonth() - 1,
+    1
+  );
+
+  const firstDayOfThisMonth = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    1
+  );
   const lastMonthRecords = await prisma.celoLiquidity.findMany({
     select: {
       total: true,
@@ -26,47 +38,76 @@ export default async function handler(
     where: {
       createdAt: {
         // >= Last month 1st day
-        gte: new Date(today.getFullYear(), today.getMonth() - 1, 1),
+        gte: firstDayOfLastMonth,
         // < First day of this month
-        lt: new Date(today.getFullYear(), today.getMonth(), 1),
+        lt: firstDayOfThisMonth,
       },
     },
   });
 
-  const breakdownEntries: { [key: string]: number[] } = {};
-  let total = 0;
+  const thisMonthRecords = await prisma.celoLiquidity.findMany({
+    select: {
+      total: true,
+      breakdown: true,
+    },
+    where: {
+      createdAt: {
+        gte: firstDayOfThisMonth,
+      },
+    },
+  });
+
+  const calc = (
+    records: {
+      total: number;
+      breakdown: Prisma.JsonValue;
+    }[],
+    days: number
+  ) => {
+    const breakdownEntries: { [key: string]: number[] } = {};
+    let total = 0;
+
+    records.forEach((record) => {
+      total += record.total;
+      Object.entries(record.breakdown || {}).forEach(([key, value]) => {
+        if (!breakdownEntries[key]) {
+          breakdownEntries[key] = [];
+        }
+        breakdownEntries[key].push(value);
+      });
+    });
+
+    total /= days;
+
+    const breakdown = Object.entries(breakdownEntries).reduce(
+      (res, [key, values]) => ({
+        ...res,
+        [key]: Math.round(values.reduce((acc, cur) => acc + cur, 0) / days),
+      }),
+      {}
+    );
+
+    return {
+      avgTotal: Math.round(total),
+      breakdown,
+    };
+  };
+
   const daysLastMonth = new Date(
     today.getFullYear(),
     today.getMonth(),
     0
   ).getDate();
-  lastMonthRecords.forEach((record) => {
-    total += record.total;
-    Object.entries(record.breakdown || {}).forEach(([key, value]) => {
-      if (!breakdownEntries[key]) {
-        breakdownEntries[key] = [];
-      }
-      breakdownEntries[key].push(value);
-    });
-  });
-
-  total /= daysLastMonth;
-
-  const breakdown = Object.entries(breakdownEntries).reduce(
-    (res, [key, values]) => ({
-      ...res,
-      [key]: Math.round(
-        values.reduce((acc, cur) => acc + cur, 0) / daysLastMonth
-      ),
-    }),
-    {}
-  );
-
   return res.status(200).json({
     yesterday,
-    lastMonth: {
-      avgTotal: Math.round(total),
-      breakdown,
+    [firstDayOfLastMonth.toLocaleString("default", { month: "long" })]: {
+      ...calc(lastMonthRecords, daysLastMonth),
     },
+    [firstDayOfThisMonth.toLocaleString("default", { month: "long" })]:
+      thisMonthRecords.length > 0
+        ? {
+            ...calc(thisMonthRecords, today.getDate()),
+          }
+        : {},
   });
 }
