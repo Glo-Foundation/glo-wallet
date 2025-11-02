108dc49687e90757797c11b52a834ffec28696e5
Rad <jaaroos@gmail.com>
2025-11-01 22:54:47 -0700

    Ubeswap usdc celo

--- pages/api/dexes.ts
+++ pages/api/dexes.ts
@@ -26,37 +26,64 @@ export default async function handler(
   return res.status(200).json(result);
 }
 
-const ADDR_CHAIN_OTHR: { [poolAddr: string]: [Chain, string, string] } = {
-  "0x13c6656a9a7c136ab651fa331e9264223f5bf9e4": [
-    mainnet,
-    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    "etherscan.io",
-  ],
-  "0x98c3648a2087df2a1c2a5b695de908bf95fa4f39": [
-    optimism,
-    "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
-    "optimistic.etherscan.io",
-  ],
-  "0x6DB8d9D795C053ad0fd24723320E47b2a21c3dC1": [
-    base,
-    "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
-    "basescan.org",
-  ],
-  "0x180bcf9197bce5a94b629289c7cd5c25329ab1c7": [
-    polygon,
-    "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
-    "polygonscan.com",
-  ],
-  "0x20f960d5b11d7b35072a38abff28ab882824c9b8": [
-    arbitrum,
-    "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-    "arbiscan.io",
-  ],
-  "0x7b9a5bc920610f54881f2f6359007957de504862": [
-    celo,
-    "0x765DE816845861e75A25fCA122bb6898B8B1282a",
-    "celoscan.io",
-  ],
+const ADDR_CHAIN_OTHR: {
+  poolAddr: string;
+  chain: Chain;
+  otherAddr: string;
+  name?: string;
+  v2Quouter?: string;
+  decimals?: number;
+}[] = [
+  {
+    poolAddr: "0x13c6656a9a7c136ab651fa331e9264223f5bf9e4",
+    chain: mainnet,
+    otherAddr: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  },
+  {
+    poolAddr: "0x98c3648a2087df2a1c2a5b695de908bf95fa4f39",
+    chain: optimism,
+    otherAddr: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+  },
+  {
+    poolAddr: "0x180bcf9197bce5a94b629289c7cd5c25329ab1c7",
+    chain: polygon,
+    otherAddr: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+  },
+  {
+    poolAddr: "0x20f960d5b11d7b35072a38abff28ab882824c9b8",
+    chain: arbitrum,
+    otherAddr: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+  },
+  {
+    poolAddr: "0x6DB8d9D795C053ad0fd24723320E47b2a21c3dC1",
+    chain: base,
+    otherAddr: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    v2Quouter: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
+  },
+  {
+    poolAddr: "0x7b9a5bc920610f54881f2f6359007957de504862",
+    chain: celo,
+    otherAddr: "0x765DE816845861e75A25fCA122bb6898B8B1282a",
+    v2Quouter: "0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8",
+    name: "Celo (cUSD)",
+    decimals: 18,
+  },
+  {
+    poolAddr: "0x28ade0134b9d0bc7041f4e5ea74fecb58504720b",
+    chain: celo,
+    otherAddr: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
+    v2Quouter: "0xA8864a18Fab1ED233Ce1921F329A6A92DBccA56f",
+    name: "Celo (USDC)",
+  },
+];
+
+const SCANNERS = {
+  celo: "celoscan.io",
+  mainnet: "etherscan.io",
+  optimism: "optimistic.etherscan.io",
+  base: "basescan.org",
+  polygon: "polygonscan.com",
+  arbitrum: "arbiscan.io",
 };
 
 const fetchBalance = async (
@@ -81,24 +108,32 @@ const collect = async ({ isCron }: { isCron: boolean }) => {
   console.log("Collecting dex data...");
   const msg: string[][] = [];
 
-  const entries = Object.entries(ADDR_CHAIN_OTHR);
-  for (const [address, [chain, usdcAddr, url]] of entries) {
+  for (const {
+    poolAddr,
+    chain,
+    otherAddr,
+    v2Quouter,
+    decimals,
+  } of ADDR_CHAIN_OTHR) {
     const chainId = chain.id;
     const gloAddr = getSmartContractAddress(chainId);
+    const url = SCANNERS[chain.name as keyof typeof SCANNERS];
 
     const result = await getPoolData({
-      poolAddr: address,
+      poolAddr,
       gloAddr,
-      usdcAddr,
+      otherAddr,
       chainId,
+      v2Quouter,
+      decimals,
     });
 
-    const glo = await fetchBalance(chainId, address, gloAddr);
-    const rawUsdc = await fetchBalance(chainId, address, usdcAddr);
-    const usdc = chainId === celo.id ? rawUsdc : rawUsdc * BigInt(1e12); // USDC has 6 decimals on most chains, but 18 on Celo
+    const glo = await fetchBalance(chainId, poolAddr, gloAddr);
+    const rawUsdc = await fetchBalance(chainId, poolAddr, otherAddr);
+    const usdc = decimals == 18 ? rawUsdc : rawUsdc * BigInt(1e12); // USDC has 6 decimals on most chains, but 18 on Celo (cUSD)
     const sum = glo + usdc;
     msg.push([
-      `*<https://${url}/address/${address}|${chain.name}>*`,
+      `*<https://${url}/address/${poolAddr}|${chain.name}>*`,
       `${result.price}$ - ${result.amountIn} USDGLO -> ${result.amountOut}`,
       `${formatPercent(glo, sum)}% / ${formatPercent(usdc, sum)}%`,
       `USDGLO: ${formatUSD(glo)}`,
@@ -108,8 +143,9 @@ const collect = async ({ isCron }: { isCron: boolean }) => {
     await sleep(2000); // to avoid rate limiting
   }
 
+  const entries = Object.values(ADDR_CHAIN_OTHR);
   const jsonResult = msg.map((elements, index) => ({
-    name: entries[index][1][0].name,
+    name: entries[index]?.name || entries[index].chain.name,
     price: elements[1],
     percentage: elements[2],
     USDGLO: elements[3],
@@ -150,12 +186,16 @@ export const getPoolData = async ({
   chainId,
   poolAddr,
   gloAddr,
-  usdcAddr,
+  otherAddr,
+  v2Quouter,
+  decimals,
 }: {
   poolAddr: string;
   gloAddr: string;
-  usdcAddr: string;
+  otherAddr: string;
   chainId: number;
+  v2Quouter?: string;
+  decimals?: number;
 }) => {
   const provider = await getJsonProvider(chainId);
 
@@ -177,14 +217,9 @@ export const getPoolData = async ({
   const amountIn = balance / BigInt(20); // 5% of the pool balance
 
   const getQuote = async () => {
-    const addr: { [chainId: number]: string } = {
-      [base.id]: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a",
-      [celo.id]: "0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8",
-    };
-
-    if (chainId in addr) {
+    if (v2Quouter) {
       const quoterContractV2 = new ethers.Contract(
-        addr[chainId],
+        v2Quouter,
         QuoterV2.abi,
         provider
       );
@@ -192,7 +227,7 @@ export const getPoolData = async ({
       const quotedAmountOut =
         await quoterContractV2.quoteExactInputSingle.staticCall({
           tokenIn: gloAddr,
-          tokenOut: usdcAddr,
+          tokenOut: otherAddr,
           fee,
           amountIn,
           sqrtPriceLimitX96: 0,
@@ -210,7 +245,7 @@ export const getPoolData = async ({
     const quotedAmountOut =
       await quoterContract.quoteExactInputSingle.staticCall(
         gloAddr,
-        usdcAddr,
+        otherAddr,
         fee,
         amountIn,
         0
@@ -219,12 +254,11 @@ export const getPoolData = async ({
     return quotedAmountOut;
   };
 
-  const isCelo = chainId === celo.id;
-  const usdcDecimals = isCelo ? 18 : 6; // USDC 6, GLO 18
+  const usdcDecimals = decimals || 6;
   const sqrtPriceX96 = slot0[0];
   const decimalsDiff = 18 - usdcDecimals;
   const fraction =
-    isGloFirst && !isCelo
+    isGloFirst && usdcDecimals == 6
       ? BigInt(2 ** 96) / BigInt(sqrtPriceX96)
       : BigInt(sqrtPriceX96) / BigInt(2 ** 96);
   const price = fraction ** BigInt(2);
